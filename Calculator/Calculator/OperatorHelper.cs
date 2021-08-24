cb03155feac2bd439bf6d1106caebb6cda5c03a8
Saman Khanum <samankhanum.sk@gmail.com>
2021-08-24 09:58:51 +0530

    Add project files.

---
+++ Calculator/Calculator/OperatorHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace Calculator
+{
+    public static class OperatorHelper
+    {
+        public static double Calculate(double value1, double value2, string myoperator)
+        {
+            double result = 0;
+            switch (myoperator)
+            {
+                case "÷":
+                    result = value1 / value2;
+                    break;
+                case "*":
+                    result = value1 * value2;
+                    break;
+                case "+":
+                    result = value1 + value2;
+                    break;
+                case "-":
+                    result = value1 - value2;
+                    break;
+              }
+            return result;
+
+        }
+
+
+    }
+
+}
